106a74120eda7ec64c9536741a92a668e3d7a11a
student-creative <aryasutariya3@gmail.com>
2025-07-19 21:17:32 +0530

    Dropdown solve

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,28 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'appearance-none',
+    'bg-[#F4F9FC]',
+    'text-slate-500',
+    'text-gray-700',
+    'border',
+    'rounded',
+    'text-sm',
+    'py-3',
+    'px-3',
+    'w-full',
+    'py-4',
+    'text-xs',
+    'text-[13px]',
+    'font-exo',
+    'font-roboto'
+  ],
   theme: {
     extend: {
-         fontFamily: {
+      fontFamily: {
         exo: ['Exo', 'sans-serif'],
-         roboto: ['Roboto', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
